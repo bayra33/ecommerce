a47a99fdc283c7e3d6d472a72c41c06f9d93afd7
bayra33 <bayra33@yahoo.com>
2024-09-10 12:46:22 +0800

    yes

---
+++ src/components/products.tsx
@@ -0,0 +1,22 @@
+export const Products = () => {
+    return(
+        <div className="flex flex-col gap-4 mb-5">
+                         <div className="grid grid-cols-4 gap-4 w-full overflow-hidden"> 
+     
+     <img className="rounded-lg w-[244px] h-[331px] object-cover transform transition-transform duration-300 hover:scale-105" src="promt.png" alt="fill" />
+     <img className="rounded-lg w-[244px] h-[331px] object-cover transform transition-transform duration-300 hover:scale-105" src="promt.png" alt="fill" />
+     <img className="rounded-lg w-[244px] h-[331px] object-cover transform transition-transform duration-300 hover:scale-105" src="promt.png" alt="fill" />
+     <img className="rounded-lg w-[244px] h-[331px] object-cover transform transition-transform duration-300 hover:scale-105" src="promt.png" alt="fill" />
+     </div>
+     <div className="grid grid-cols-4 gap-4 w-full overflow-hidden"> 
+     
+     <img className="rounded-lg w-[244px] h-[331px] object-cover transform transition-transform duration-300 hover:scale-105" src="promt.png" alt="fill" />
+     <img className="rounded-lg w-[244px] h-[331px] object-cover transform transition-transform duration-300 hover:scale-105" src="promt.png" alt="fill" />
+     <img className="rounded-lg w-[244px] h-[331px] object-cover transform transition-transform duration-300 hover:scale-105" src="promt.png" alt="fill" />
+     <img className="rounded-lg w-[244px] h-[331px] object-cover transform transition-transform duration-300 hover:scale-105" src="promt.png" alt="fill" />
+     </div>
+     
+        </div>
+        
+    )
+}
